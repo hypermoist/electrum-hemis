bb5089a1817ad7e8b8c329080ea730792277ca17
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-05-07 17:56:03 +0100

    android: Add algomodule to the recipes

--- contrib/android/p4a_recipes/algomodule/__init__.py
+++ contrib/android/p4a_recipes/algomodule/__init__.py
@@ -3,5 +3,6 @@ from pythonforandroid.recipe import CythonRecipe
 class AlgoModuleRecipe(CythonRecipe):
     version = "1.1.0"
     url = 'https://github.com/electrum-altcoin/AlgoLib/archive/{version}.zip'
+    depends = ["setuptools", "wheel"]
 
 recipe = AlgoModuleRecipe()
