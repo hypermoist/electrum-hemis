689a4ecbfb5bd7a2f301dc372a630703bb96a02c
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-05-09 20:59:00 +0100

    android: Use AlgoModule 1.1.1

--- contrib/android/p4a_recipes/algomodule/__init__.py
+++ contrib/android/p4a_recipes/algomodule/__init__.py
@@ -1,22 +1,8 @@
 from pythonforandroid.recipe import CythonRecipe
 
 class AlgoModuleRecipe(CythonRecipe):
-    version = "1.1.0"
+    version = "1.1.1"
     url = 'https://github.com/electrum-altcoin/AlgoLib/archive/{version}.zip'
-    depends = ['setuptools', 'openssl']
-    call_hostpython_via_targetpython = False
-
-    def get_recipe_env(self, arch, with_flags_in_cc=True):
-        """
-        Adds openssl recipe to include and library path.
-        """
-        env = super().get_recipe_env(arch, with_flags_in_cc)
-        openssl_recipe = self.get_recipe('openssl', self.ctx)
-        env['CFLAGS'] += openssl_recipe.include_flags(arch)
-        env['LDFLAGS'] += ' -L{}'.format(self.ctx.get_libs_dir(arch.arch))
-        env['LDFLAGS'] += ' -L{}'.format(self.ctx.libs_dir)
-        env['LDFLAGS'] += openssl_recipe.link_dirs_flags(arch)
-        env['LIBS'] = env.get('LIBS', '') + openssl_recipe.link_libs_flags()
-        return env
+    depends = ['setuptools']
 
 recipe = AlgoModuleRecipe()
