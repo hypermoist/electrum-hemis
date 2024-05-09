43b578d87767720303e8c2561e1e66eabbbff2c4
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-05-09 10:58:34 +0100

    fix: Another attempt at fixing AlgoLib

--- contrib/android/p4a_recipes/algomodule/__init__.py
+++ contrib/android/p4a_recipes/algomodule/__init__.py
@@ -3,6 +3,20 @@ from pythonforandroid.recipe import CythonRecipe
 class AlgoModuleRecipe(CythonRecipe):
     version = "1.1.0"
     url = 'https://github.com/electrum-altcoin/AlgoLib/archive/{version}.zip'
-    depends = ["setuptools", "wheel"]
+    depends = ['setuptools', 'openssl']
+    call_hostpython_via_targetpython = False
+
+    def get_recipe_env(self, arch, with_flags_in_cc=True):
+        """
+        Adds openssl recipe to include and library path.
+        """
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        openssl_recipe = self.get_recipe('openssl', self.ctx)
+        env['CFLAGS'] += openssl_recipe.include_flags(arch)
+        env['LDFLAGS'] += ' -L{}'.format(self.ctx.get_libs_dir(arch.arch))
+        env['LDFLAGS'] += ' -L{}'.format(self.ctx.libs_dir)
+        env['LDFLAGS'] += openssl_recipe.link_dirs_flags(arch)
+        env['LIBS'] = env.get('LIBS', '') + openssl_recipe.link_libs_flags()
+        return env
 
 recipe = AlgoModuleRecipe()
