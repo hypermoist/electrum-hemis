c740fed48afe9a1bc3445880e8f6049192dc813c
Ariel Marshall-Smith <jacobms109@gmail.com>
2024-05-15 22:33:32 +1200

    ah

--- electrum_hms/plugins/ledger/ledger.py
+++ electrum_hms/plugins/ledger/ledger.py
@@ -597,18 +597,15 @@ class LedgerPlugin(HW_PluginBase):
                    (0x2581, 0x3b7c), # HW.1 ledger production
                    (0x2581, 0x4b7c), # HW.1 ledger test
                    (0x2c97, 0x0000), # Blue
-                   (0x2c97, 0x0011), # Blue app-bitcoin >= 1.5.1
-                   (0x2c97, 0x0015), # Blue app-bitcoin >= 1.5.1
                    (0x2c97, 0x0001), # Nano-S
-                   (0x2c97, 0x1011), # Nano-S app-bitcoin >= 1.5.1
-                   (0x2c97, 0x1015), # Nano-S app-bitcoin >= 1.5.1
                    (0x2c97, 0x0004), # Nano-X
-                   (0x2c97, 0x4011), # Nano-X app-bitcoin >= 1.5.1
-                   (0x2c97, 0x4015), # Nano-X app-bitcoin >= 1.5.1
-                   (0x2c97, 0x0005), # Nano-S-Plus
-                   (0x2c97, 0x5011), # Nano-S-Plus app-bitcoin >= 1.5.1
-                   (0x2c97, 0x5015), # Nano-S-Plus app-bitcoin >= 1.5.1
-                ]
+                   (0x2c97, 0x0005), # Nano-S Plus
+                   (0x2c97, 0x0006), # Stax
+                   (0x2c97, 0x0007), # RFU
+                   (0x2c97, 0x0008), # RFU
+                   (0x2c97, 0x0009), # RFU
+                   (0x2c97, 0x000a)  # RFU
+                 ]
     VENDOR_IDS = (0x2c97,)
     LEDGER_MODEL_IDS = {
         0x10: "Ledger Nano S",
